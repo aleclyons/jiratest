3d777dd742e4847538cfadf0bc8852c4892a5e5f
Alec Lyons <alec.lyons@bcsg.com>
2012-11-23 18:02:51 +0000

    Making some changes for bug JTP-13
    I have modified the README, and added a sophisticated Java application here.  With a bit of luck, this should integrate with JIRA

---
+++ sourcecode.java
@@ -0,0 +1,13 @@
+/**
+* Author: Alec Lyons
+* Description: Amazingly sophisticated application that will change the world.
+*
+*/
+class sourcecode
+{  
+        public static void main(String args[])
+        {
+           System.out.println("Hello World!");
+        }
+}
+
